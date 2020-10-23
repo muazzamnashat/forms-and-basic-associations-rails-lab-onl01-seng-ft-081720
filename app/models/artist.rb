57f6b2730b48ad9c8ba5967263821cfa0deb216e
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-10-23 05:43:31 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,7 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genre, through: :songs
+  has_many :notes, through: :songs
+
   # add associations here
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
   # add associations here
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    # binding.pry
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def song_notes=(notes)
+    notes.each do |note|
+      new_note = Note.find_or_create_by(content: note)
+      self.notes << new_note
+    end
+  end
 end
